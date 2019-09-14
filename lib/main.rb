30bde84441405cb59be2e6360265bb65330bacd6
Marvellous Ubani <marvellousubani@gmail.com>
2019-09-14 10:29:52 +0100

    Update main.rb

--- lib/main.rb
+++ lib/main.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-
 module Enumerable
   def my_each
     i = 0
@@ -11,55 +10,69 @@ module Enumerable
   end
 
   def my_each_with_index
-    for i in 0...size
+    i = 0
+    while i < size
       yield(self[i], i)
+      i += 1
     end
   end
 
   def my_select
     new_arr = []
-    for i in 0...size
+    i = 0
+    while i < size
       new_arr << self[i] if yield(self[i]) == true
+      i += 1
     end
     return new_arr
   end
 
   def my_all
     pos = 0
-    for i in 0...size
+    i = 0
+    while i < size
       pos += 1 if yield(self[i]) == true
+      i += 1
     end
     pos == self.size
   end
 
   def my_none
     pos = 0
-    for i in 0...size
+    i = 0
+    while i < size
       pos += 1 if yield(self[i]) == false
+      i += 1
     end
     pos == self.size
   end
 
   def my_count
     count = 0
-    for i in 0...size
+    i = 0
+    while i < size
       count +=1 if yield(self[i]== true)
+      i += 1
     end
     return count
   end
 
   def my_map(&block)
     new_arr = []
-    for i in 0...size
+    i = 0
+    while i < size
       new_arr << block.call(self[i])
+      i += 1
     end
     return new_arr
   end
 
   def my_inject
     final = self[0]
-    for i in 1...size
+    i = 0
+    while i < size
       final = yield(final,self[i])
+      i += 1
     end
     return final
   end
@@ -70,7 +83,5 @@ def multiply_els(arr)
   arr.my_inject{|total, multiple| total * multiple}
 end
 
-[1,2,3].my_each do |num| 
-  puts num
-end
+
 
