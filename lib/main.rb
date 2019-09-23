e8982be3b2dd3ebcd51045f1a593ef26b138a19e
Marvellous Ubani <marvellousubani@gmail.com>
2019-09-23 21:00:28 +0100

    Update main.rb

--- lib/main.rb
+++ lib/main.rb
@@ -59,27 +59,24 @@ module Enumerable
 
   def my_all?(arg = nil)
     unless block_given?
-      falsy = 0
-      if arg.nil?
-        my_each do |x|
-          falsy += 1 if x == false || x.nil?
-        end
-      else
-        # if arg has a value, if its a number
-        if arg.class == Class
+        falsy = 0
+        if arg.nil? 
           my_each do |x|
-            falsy += 1 unless x.is_a?(arg)
+            falsy += 1 if x == false || x == nil
           end
+        elsif arg.class == Class
+            my_each do |x|
+              falsy += 1 if !x.is_a?(arg)
+            end
         elsif arg.class == Regexp
-          num = 0
-          my_each do |x|
-            num += 1 if x.match(arg)
-          end
-          return num == size
+            num = 0
+            my_each do |x|
+              num += 1 if x.match(arg)
+            end
+            return num == size
         end
-      end
-      return falsy.zero?
-      return self == [arg]
+        return falsy.zero?
+        return self == [arg] 
     end
 
     pos = 0
